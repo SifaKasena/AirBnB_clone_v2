c05d0441e94f905d474416a84f42d59756a823b2
SifaKasena <sifa.collins@gmail.com>
2023-06-21 16:32:15 +0300

    Update console to save new instances

--- console.py
+++ console.py
@@ -142,9 +142,8 @@ class HBNBCommand(cmd.Cmd):
                         setattr(new_instance, attr_name, float(attr_value))
                     else:
                         setattr(new_instance, attr_name, attr_value)
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
