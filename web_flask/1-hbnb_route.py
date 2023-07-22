b0dbe9536c19abb4e3a47d46d5dd1a05b3616d1a
Michael Hudson <mnkotagu@gmail.com>
2023-07-22 10:49:52 +0300

    Fix URL typo in 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -18,7 +18,7 @@ def hello():
     return ("Hello HBNB!")
 
 
-@app.route('/HBNB', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return HBNB"""
     return ("HBNB")
