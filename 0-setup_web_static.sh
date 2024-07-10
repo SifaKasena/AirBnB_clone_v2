0a1350c619bb8393ed72eb065730a5af584e849d
SifaKasena <sifa.collins@gmail.com>
2024-07-10 21:02:16 +0300

    [Fix:] Requirements

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "Test Html" > /data/web_static/releases/test/index.html
+sudo echo "Test Html" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '$c\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n}' /etc/nginx/sites-available/default
