bdb6fd0278c01e8f9cf01ee741f10c326a4e5331
SifaKasena <sifa.collins@gmail.com>
2024-07-11 10:59:47 +0300

    Rewrite

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ Usage: fab -f 2-do_deploy_web_static.py do_deploy:<ARCHIVE_PATH>
 import os
 from fabric.api import env, put, run
 
-env.hosts = ['18.234.106.203', '54.236.12.250']
+env.hosts = ["18.234.106.203", "54.236.12.250"]
 
 
 def do_deploy(archive_path):
@@ -21,7 +21,7 @@ def do_deploy(archive_path):
         bool: True if the deployment was successful, False otherwise.
     """
     try:
-        if not os.path.exists(archive_path):
+        if not (os.path.exists(archive_path)):
             return False
 
         put(archive_path, "/tmp/")
@@ -29,14 +29,25 @@ def do_deploy(archive_path):
         archive_filename = os.path.basename(archive_path)
         archive_name = os.path.splitext(archive_filename)[0]
 
-        run(f"sudo mkdir -p /data/web_static/releases/{archive_name}/")
-        run(f"sudo tar -xzf /tmp/{archive_name}.tgz -C\
-            /data/web_static/releases/{archive_name}/")
-        run(f"sudo rm -rf /tmp/{archive_name}.tgz")
-        run("sudo rm -rf /data/web_static/current")
-        run(f"sudo ln -sf /data/web_static/releases/{archive_name}\
-            /data/web_static/current")
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(archive_name))
 
+        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+            format(archive_name, archive_name))
+
+        run("sudo rm /tmp/{}.tgz".format(archive_name))
+
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
+         /data/web_static/releases/{}/".
+            format(archive_name, archive_name))
+
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_name))
+
+        run("sudo rm -rf /data/web_static/current")
+
+        run("sudo ln -s /data/web_static/releases/{}/\
+         /data/web_static/current".
+            format(archive_name))
     except Exception:
         return False
 
